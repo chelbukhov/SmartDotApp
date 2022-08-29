16939bcbe723dc34ca3d601112e1390a433072a7
Chelbukhov <chelbukhov@gmail.com>
2022-08-29 07:53:06 +0300

    add send transaction - add additional record for item

--- app/src/main/Item.js
+++ app/src/main/Item.js
@@ -23,6 +23,12 @@ const Item =(props) => {
       return initialState;
   
     });
+    const [addRecord, setRecord] = useState('');
+
+    function setAddRecord (event) {
+      console.log('event:', event);
+      setRecord(event);
+    }
 
     async function getItem() {
         const geoMultiplier = await contract.methods.geoMultiplier().call();
@@ -79,7 +85,13 @@ const Item =(props) => {
             //console.log('createDate: ', timeConverter(createDate.timestamp));
   
             //console.log('result event: ', result[0].returnValues.description);
-            addRecords.push(<li key = {addRecords.length}>{timeConverter(createDate.timestamp)} {result[0].returnValues.description}</li>);
+//            addRecords.push(<li key = {addRecords.length}>{timeConverter(createDate.timestamp)} {result[0].returnValues.description}</li>);
+            addRecords.push(
+              <tr key = {addRecords.length}>
+                <td> {timeConverter(createDate.timestamp)}</td>
+                <td> {result[0].returnValues.description}</td>
+              </tr>
+              );
   
           }
           else {
@@ -101,24 +113,61 @@ const Item =(props) => {
     useEffect(() => {
       showItem();
     });
-  //start point
   
+    function formSubmit (event){
+      console.log(event);
+      event.preventDefault();
+      sendTransaction();
+    }
 
-
+    async function sendTransaction() {
+      if(addRecord.length < 5 ){
+        alert('Mimimum length of record is 5 signs.')
+      }
+        else
+      {
+        await contract.methods.addRecord(param.itemID, addRecord).send({
+          from: defaultAccount
+        }); 
+        // refresh data
+        setItem(getItem());
+        setRecord('');
+      }
+    }
   
+  
+    //start point
+   
     return (
         <div>
           <Header />
           <h3><NavLink to={`/showCollection/collectionID=${param.collectionID}/address=${param.address}/account=${param.account}`}>Back</NavLink></h3>
           <div className="item">
             <h2>{item.nameItem}</h2>
-            <img className="item" src={item.img} alt='' />
-            <h3>{item.description}</h3>
-            <h3>Date of creation: {item.createDate}</h3>
-            <h3>Longitude: {item.longitude}</h3>
-            <h3>Latitude: {item.latitude}</h3>
-            <h3>Additional records in blockchain:</h3>
-            <p>{item.additionalRecords}</p>
+            <div>
+              <div>
+                <img className="item" src={item.img} alt='' />
+              </div>
+              <div className="itemtext">
+                <h3>{item.description}</h3>
+                <h3>Date of creation: {item.createDate}</h3>
+                <h3>Longitude: {item.longitude}</h3>
+                <h3>Latitude: {item.latitude}</h3>
+                <h3>Additional records in blockchain:</h3>
+                <table >{item.additionalRecords}</table>
+                <form onSubmit={formSubmit}>
+                    <textarea 
+                      id='addRecordText' 
+                      rows="1" 
+                      cols={document.body.clientWidth/8-6} 
+                      placeholder="Enter additional information. Minimum is 5 signs. Maximum is 1000 signs for one record." 
+                      maxLength={1000}
+                      onChange={(e) => setAddRecord(e.target.value)}
+                    />
+                    <input type="submit" value="Send transaction" />
+                </form>
+              </div>
+            </div>
           </div>
           <Footer />
         </div>
